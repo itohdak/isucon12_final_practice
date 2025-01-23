a344b1fa1d358631769f1f96f37703348f0c56f2
itohdak <itohdak@gmail.com>
2025-01-24 00:01:38 +0900

    modify index

--- webapp/sql/setup/99_setup_patch.sql
+++ webapp/sql/setup/99_setup_patch.sql
@@ -1,2 +1,2 @@
 ALTER TABLE `user_present_all_received_history` ADD INDEX idx_user_id_present_all_id (`user_id`, `present_all_id`);
-ALTER TABLE `user_presents` ADD INDEX idx_user_id_created_at_desc_id_deleted_at (`user_id`, `created_at` DESC, `id`, `deleted_at`);
+ALTER TABLE `user_presents` ADD INDEX idx_user_id_deleted_at_created_at_desc_id (`user_id`, `deleted_at`, `created_at` DESC, `id`);
