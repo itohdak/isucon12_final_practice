65444dcd0ed0d5bcaa469d59f8e9210899ed0874
itohdak <itohdak@gmail.com>
2025-01-23 21:47:48 +0900

    fix patch

--- webapp/sql/setup/setup.sh
+++ webapp/sql/setup/setup.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
-set -x
+set -ex
 cd `dirname $0`
 
-ISUCON_DB_HOST=${ISUCON_DB_HOST:-localhost}
+ISUCON_DB_HOST=${ISUCON_DB_HOST:-127.0.0.1}
 ISUCON_DB_PORT=${ISUCON_DB_PORT:-3306}
 ISUCON_DB_USER=${ISUCON_DB_USER:-isucon}
 ISUCON_DB_PASSWORD=${ISUCON_DB_PASSWORD:-isucon}
 ISUCON_DB_NAME=${ISUCON_DB_NAME:-isucon}
-MYSQL_ROOT_PASSWORD=${MYSQL_ROOT_PASSWORD:-root}
 
-sudo mysql -uroot \
-		-p"$MYSQL_ROOT_PASSWORD" \
+mysql -u"$ISUCON_DB_USER" \
+		-p"$ISUCON_DB_PASSWORD" \
 		--host "$ISUCON_DB_HOST" \
-		--port "$ISUCON_DB_PORT"  < 99_setup_patch.sql
+		--port "$ISUCON_DB_PORT" \
+		"$ISUCON_DB_NAME" < 99_setup_patch.sql
